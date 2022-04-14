41341d5f6c96e66621727b345c4dacfb94074f34
ItamarSahar <91605437+ItamarSahar@users.noreply.github.com>
2022-04-14 12:30:36 +0300

    Add files via upload

--- js/upload-service.js
+++ js/upload-service.js
@@ -39,3 +39,25 @@ function doUploadImg(imgDataUrl, onSuccess) {
 			console.error(err)
 		})
 }
+
+function onImgInput(ev) {
+	loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+	var reader = new FileReader()
+
+	reader.onload = (event) => {
+		console.log('onload')
+		var img = new Image()
+		// Render on canvas
+		img.src = event.target.result
+		img.onload = onImageReady.bind(null, img)
+	}
+	console.log('after')
+	reader.readAsDataURL(ev.target.files[0])
+}
+
+function renderImg(img) {
+	gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
